be7c806180896a3b631b6e63020e074ffbb0df94
frama21 <doxs21@gmail.com>
2014-09-23 19:47:09 +0200

    comit prueba

--- app/src/main/java/com/example/ruben/easytransport/MainActivity.java
+++ app/src/main/java/com/example/ruben/easytransport/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 //PRImer cambio
+    //Segundo cambio jnsñlknaslkfasdf
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
