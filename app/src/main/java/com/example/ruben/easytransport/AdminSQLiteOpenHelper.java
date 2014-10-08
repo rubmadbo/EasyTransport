c27cfcc21f956221522ab4cf401afa6429dd5b9d
frama21 <doxs21@gmail.com>
2014-10-08 16:38:07 +0200

    Base de datos sqlite con onUpgrade

--- app/src/main/java/com/example/ruben/easytransport/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/ruben/easytransport/AdminSQLiteOpenHelper.java
@@ -32,7 +32,23 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     //borrar la tabla y crear la nueva tabla
     @Override
     public void onUpgrade(SQLiteDatabase db, int versionAnte, int versionNue) {
-        db.execSQL("drop table if exists votantes");
-        db.execSQL("create table votantes(dni integer primary key, nombre text, colegio text, nromesa integer)");
+        db.execSQL("drop table if exists Acuerdo");
+        db.execSQL("drop table if exists Ruta");
+        db.execSQL("drop table if exists TipoUsuario");
+        db.execSQL("drop table if exists Usuarios");
+        db.execSQL("drop table if exists Vehiculo");
+
+        db.execSQL("create table Acuerdo(idAcuerdo integer primary key, usuarioTransportista integer, " +
+                "usuarioCliente integer, ruta integer, precio integer, comentario text)");
+
+        db.execSQL("create table Ruta(idRuta integer primary key, inicio text, final text, horaInicio text, horaFinal text" +
+                "fecha text, comentarios text, transportista integer)");
+
+        db.execSQL("create table TipoUsuario(idTipoUsuario integer primary key, nombre text, descripcion text)");
+
+        db.execSQL("create table Usuarios(DNI integer primary key, nombre text, apellidos text, direccion text, telefono integer, " +
+                "alias text, contrasenya text, tipoUsuario integer, vehiculo integer)");
+
+        db.execSQL("create table Vehiculo(matricula text primary key, marca text, modelo text)");
     }
 }
