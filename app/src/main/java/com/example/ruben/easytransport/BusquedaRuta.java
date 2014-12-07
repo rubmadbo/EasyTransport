16202572b5026bc504853926d32222de3636244c
ADRIAN GOMEZ <adrian4192@gmail.com>
2014-12-07 19:47:08 +0100

    Merge branch 'master' of /Users/adrian_home/Desktop/EasyTransport/EasyTransport with conflicts.

--- app/src/main/java/com/example/ruben/easytransport/BusquedaRuta.java
+++ app/src/main/java/com/example/ruben/easytransport/BusquedaRuta.java
@@ -1,5 +1,6 @@
 package com.example.ruben.easytransport;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,12 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
+import android.widget.Spinner;
 
 public class BusquedaRuta extends Fragment {
 
@@ -24,9 +20,8 @@ public class BusquedaRuta extends Fragment {
     private EditText destino=null;
     private DatePicker _date=null;
     private DatePicker _date2=null;
-
-
-
+    private Spinner lista;
+    private Spinner lista1;
 
 
     @Override
@@ -48,35 +43,24 @@ public class BusquedaRuta extends Fragment {
                 _date= (DatePicker)rootView.findViewById(R.id.datePicker);
                 _date2= (DatePicker)rootView.findViewById(R.id.datePicker2);
 
-
-
                 int mes = _date.getMonth();
                 int year= _date.getYear();
                 int dia = _date.getDayOfMonth();
-                GregorianCalendar fechaDesdeSeleccionada = new GregorianCalendar(year,mes,dia);
-                Date fechaDesde = fechaDesdeSeleccionada.getTime();
                 String _fecha = String.format("%d/%d/%d", dia, mes+1, year);
 
                 int mes_2 = _date2.getMonth();
                 int year_2= _date2.getYear();
                 int dia_2 = _date2.getDayOfMonth();
-                GregorianCalendar fechaHastaSeleccionada = new GregorianCalendar(year_2,mes_2,dia_2);
-                Date fechaHasta= fechaHastaSeleccionada.getTime();
                 String _fecha2 = String.format("%d/%d/%d", dia_2, mes_2+1, year_2);
 
-                Calendar c = Calendar.getInstance();
-                int actualYear = c.get(Calendar.YEAR);
-                int actualMonth = c.get(Calendar.MONTH);
-                int actualDay = c.get(Calendar.DAY_OF_MONTH);
-                GregorianCalendar calendarHoy = new GregorianCalendar(actualYear,actualMonth,actualDay);
-                Date fechaActual = calendarHoy.getTime();
 
 
 
 
 
-                if(_date!=null && _date2!=null && fechaDesde.getTime() <= fechaHasta.getTime() &&
-                        fechaActual.getTime() <= fechaDesde.getTime() && fechaActual.getTime() <= fechaHasta.getTime()) {
+
+                if(_date!=null && _date2!=null && _orig.length()>0 && year<=year_2) {
+                    //Toast.makeText(this, "Realizando la búsqueda", Toast.LENGTH_SHORT).show();
 
                     Intent intent = new Intent(getActivity().getBaseContext(),ListarRuta.class);
                     intent.putExtra("Des",_dest );
@@ -86,18 +70,29 @@ public class BusquedaRuta extends Fragment {
                     getActivity().startActivity(intent);
 
                 }
-                else if(_date==null || _date2==null || _fecha==null || _fecha2==null  || _dest.length()<=0 || _orig.length()<=0) {
-                    Toast.makeText(getActivity(), "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+                if(_date==null || _date2==null || _fecha==null || _fecha2==null  || _dest.length()<=0 || _orig.length()<=0) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                    dialog.setMessage("Completa todos los campos");
+                    dialog.setCancelable(true);
+                    dialog.show();
+
+
+
                 }
-                else if(_date!=null && _date2!=null && fechaActual.getTime() > fechaDesde.getTime() || fechaActual.getTime() > fechaHasta.getTime()){
-                    Toast.makeText(getActivity(), "Compruebe que las fechas no sean inferiores a la actual", Toast.LENGTH_SHORT).show();
-                }
-                else if(_date!=null && _date2!=null && fechaHasta.getTime() <= fechaDesde.getTime()) {
-                    Toast.makeText(getActivity(), "La fecha de inicio no puede ser mayor que la de fin", Toast.LENGTH_SHORT).show();
+                if(_date!=null && _date2!=null && _fecha.compareTo(_fecha2)>0 && _dest.length()>0 && _orig.length()>0 && year>year_2) {
+
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                    dialog.setMessage("La fecha de inicio no puede ser mayor que la final");
+                    dialog.setCancelable(true);
+                    dialog.show();
                 }
                 //hay que arreglar el equals del origen destino
-                else if(_date!=null && _date2!=null && _fecha.compareTo(_fecha2)>0 && _dest.length()>0 && _orig.length()>0 && year>year_2 && _dest.equalsIgnoreCase(_orig)) {
-                    Toast.makeText(getActivity(), "El origen no puede ser igual al destino", Toast.LENGTH_SHORT).show();
+                if(_date!=null && _date2!=null && _fecha.compareTo(_fecha2)>0 && _dest.length()>0 && _orig.length()>0 && year>year_2 && _dest.equalsIgnoreCase(_orig)) {
+
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(v.getContext());
+                    dialog.setMessage("El destino no puede ser igual al destino");
+                    dialog.setCancelable(true);
+                    dialog.show();
                 }
 
 
