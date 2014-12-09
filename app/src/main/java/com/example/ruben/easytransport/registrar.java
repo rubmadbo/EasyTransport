c7bc8179c93b961c6b3936e224898e3b68ebf8ab
marietatu <marietatu@free.fr>
2014-12-09 18:44:04 +0100

    Rutas Favoritas 2

---
+++ app/src/main/java/com/example/ruben/easytransport/registrar.java
@@ -0,0 +1,90 @@
+package com.example.ruben.easytransport;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class registrar extends ActionBarActivity {
+
+    //Faire la meme chose que busquedaRuta
+    private Button botonRegistrar;
+    private EditText Nombre=null;
+    private EditText Apellidos=null;
+    private EditText Email=null;
+    private EditText Contrasena=null;
+    private EditText RepContrasena=null;
+    private CheckBox VerContrasena=null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        final Context context = getApplicationContext();
+
+        // Si pusemos el boton :
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Nombre = (EditText) findViewById(R.id.EntrarNombre);
+                Apellidos = (EditText) findViewById(R.id.EntrarApellidos);
+                Email = (EditText) findViewById(R.id.EntrarEmail);
+                Contrasena = (EditText) findViewById(R.id.EntrarContrasena);
+                RepContrasena = (EditText) findViewById(R.id.RepetirContrasena);
+                VerContrasena = (CheckBox) findViewById(R.id.ContrasenaVisible);
+
+                String _Nom = Nombre.getText().toString();
+                String _Apell = Apellidos.getText().toString();
+                String _Email = Email.getText().toString();
+                String _Contrasena = Contrasena.getText().toString();
+                String _RepContrasena = RepContrasena.getText().toString();
+
+                if(_Nom!=null && _Apell!=null && _Email!=null && _Contrasena!=null && _RepContrasena!=null) {
+
+                    // A ver lo que es el intent
+                    /// Cambiar el intent
+                   /* Intent intent = new Intent(this,LoginFirst.class);
+                    intent.putExtra("Nom",_Nom );
+                    intent.putExtra("Apell",_Apell );
+                    intent.putExtra("Email",_Email );
+                    intent.putExtra("Cont",_Contrasena );
+                    intent.putExtra("RepCont",_RepContrasena );
+                    getActivity().startActivity(intent);*/
+
+                }
+                else if(_Nom==null || _Apell==null || _Email==null || _Contrasena==null || _RepContrasena==null) {
+                    Toast.makeText(context, "Rellene todos los campos", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+
+
+            }
+
+        });
+
+
+    }
+
+
+
+}
