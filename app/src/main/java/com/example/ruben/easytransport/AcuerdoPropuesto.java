f30a9c802b1adf645fbf77191cb85862580dce5c
frama21 <doxs21@gmail.com>
2014-12-09 13:43:23 +0100

    Cambios en aceptar y rechazar acuerdo

--- app/src/main/java/com/example/ruben/easytransport/AcuerdoPropuesto.java
+++ app/src/main/java/com/example/ruben/easytransport/AcuerdoPropuesto.java
@@ -4,14 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class AcuerdoPropuesto extends ActionBarActivity {
+
+    private RadioButton radioRechazar;
+    private RadioButton radioAceptar;
+    private TextView TmotivoRechazo;
+    private EditText ETmotivoRechazo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acuerdo_propuesto);
+
+        radioRechazar = (RadioButton) findViewById(R.id.radioRechazar);
+        radioAceptar = (RadioButton) findViewById(R.id.radioAceptar);
+        TmotivoRechazo = (TextView) findViewById(R.id.textMotivoRechazo);
+        ETmotivoRechazo = (EditText) findViewById(R.id.editTRechazo);
+
+        radioRechazar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TmotivoRechazo.setVisibility(View.VISIBLE);
+                ETmotivoRechazo.setVisibility(View.VISIBLE);
+                Toast.makeText(AcuerdoPropuesto.this, "Botón rechazar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        radioAceptar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TmotivoRechazo.setVisibility(View.INVISIBLE);
+                ETmotivoRechazo.setVisibility(View.INVISIBLE);
+               Toast.makeText(AcuerdoPropuesto.this, "Botón aceptar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
